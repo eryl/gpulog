4d18ad60d0cbee013f146b15237a9124a45ace7c
Erik Ylipää <erik.ylipaa@gmail.com>
2021-02-15 09:51:16 +0100

    Allow filtering of GPUs

--- plot_nvidia_dump.py
+++ plot_nvidia_dump.py
@@ -8,11 +8,23 @@ import matplotlib.pyplot as plt
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('statfile', type=Path)
+    parser.add_argument('--filter-ids', help="If given, only display GPU's with these ids (numbered after bus id).", type=int, nargs='+')
     args = parser.parse_args()
 
     stats = pd.read_csv(args.statfile, parse_dates=True, skipinitialspace=True, index_col=0)
     for col_name in ('utilization.gpu [%]', 'utilization.memory [%]'):
         stats[col_name] = stats[col_name].str.rstrip(' %').astype(float) / 100
+
+    bus_ids = {bus_id:i for i,bus_id in enumerate(sorted(stats['pci.bus_id'].unique()))}
+
+    stats['gpu_id'] = [bus_ids[bus_id] for bus_id in stats['pci.bus_id']]
+    if args.filter_ids:
+        to_drop = set(bus_ids.values()) - set(args.filter_ids)
+        for i in to_drop:
+            stats = stats.drop(stats[stats['gpu_id'] == i].index)
+
+    if len(stats) < 1:
+        raise RuntimeError("No values to display, did you filter out all GPU ids?")
 
     fig, (ax_compute, ax_mem) = plt.subplots(2, 1, sharex='col')
     stats.groupby('pci.bus_id')['utilization.gpu [%]'].plot(ax=ax_compute, legend=True)
